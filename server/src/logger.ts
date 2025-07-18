81483e380ca2ab8fa6af393e3b580c215866455f
Aleh Kashnikau <aleh.kashnikau@gmail.com>
2025-07-18 23:59:44 +0200

    Disable log to file

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -5,12 +5,6 @@ export const logger = pino(
 	pino.transport({
 		targets: [
 			{
-				target: "pino/file",
-				options: {
-					destination: path.resolve(import.meta.dir, "../var/dev.log"),
-				},
-			},
-			{
 				target: "pino-pretty",
 			},
 		],
