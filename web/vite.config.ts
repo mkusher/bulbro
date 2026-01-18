edfe2493a46ada79ffc0f99062c0ce204b1fb32f
Aleh Kashnikau <aleh.kashnikau@gmail.com>
2026-01-18 20:36:08 +0100

    Update web packages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,15 +3,29 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { defineConfig } from "rolldown-vite";
 
-export default defineConfig({
-	plugins: [tailwindcss(), preact()],
-	build: {
-		outDir: "../server/public",
+export default defineConfig(
+	{
+		plugins:
+			[
+				tailwindcss(),
+				preact(),
+			],
+		build:
+			{
+				outDir:
+					"../server/public",
+			},
+		resolve:
+			{
+				alias:
+					{
+						"@": path.resolve(
+							__dirname,
+							"src",
+						),
+					},
+			},
+		appType:
+			"spa",
 	},
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "src"),
-		},
-	},
-	appType: "spa",
-});
+);
